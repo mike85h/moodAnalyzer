856ee37462f37f01be3b48803f29d0f4e03f5052
Michael Harrington <mike85h@gmail.com>
2017-06-26 10:08:45 -0600

    nt sure how this is working but it is. run at localhost:8080

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 //created by Michael Harrington
-
 //encodeURI("string here"); will output string of type needed by watson
 
-console.log("Working MH");
+var unique = require('uniq');
+
+var encodedInput = "";
 
 $('#mainButton').click(function(){
   var input = $('#mainInput').val();
-  alert(input);
-})
+  encodedInput = encodeURI(input);
+  encodedInput = '"' + encodedInput + '"'
+  console.log(encodedInput);
 
-// function getMood(){
-//   var input = document.getElementById("mainInput").innerHTML;
-//   console.log(input);
-//   console.log("getMoodWorking");
-// };
+// {
+//   "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+//   "username": "aa44c815-f9e5-45ec-9862-86133587c17a",
+//   "password": "Zzgmqx7IQjf8"
+// }
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3.js');
+var tone_analyzer = new ToneAnalyzerV3({
+  username: "d9eebd1b-b1a9-4cd9-94e3-714537281c54",
+  password: "O7yKSnOt2zIM",
+  version_date: '2016-05-19'
+});
+
+console.log(encodedInput);
+var params = {
+  text: encodedInput,
+  tones: 'emotion'
+};
+
+tone_analyzer.tone(params, function(error, response) {
+  if (error)
+    console.log('error:', error);
+  else
+    console.log(JSON.stringify(response, null, 2));
+    $('.responseText').val(response);
+  }
+);
+
+});
