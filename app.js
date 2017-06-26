eccf7d9e432f2143b42316022704d77f6fe33ec3
Michael Harrington <mike85h@gmail.com>
2017-06-26 12:29:38 -0600

    added style and return json. all working

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //created by Michael Harrington
 //encodeURI("string here"); will output string of type needed by watson
-
 var unique = require('uniq');
 
 var encodedInput = "";
@@ -9,13 +8,14 @@ $('#mainButton').click(function(){
   var input = $('#mainInput').val();
   encodedInput = encodeURI(input);
   encodedInput = '"' + encodedInput + '"'
-  console.log(encodedInput);
+  // console.log(encodedInput);
 
 // {
 //   "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
 //   "username": "aa44c815-f9e5-45ec-9862-86133587c17a",
 //   "password": "Zzgmqx7IQjf8"
 // }
+
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3.js');
 var tone_analyzer = new ToneAnalyzerV3({
   username: "d9eebd1b-b1a9-4cd9-94e3-714537281c54",
@@ -23,18 +23,24 @@ var tone_analyzer = new ToneAnalyzerV3({
   version_date: '2016-05-19'
 });
 
-console.log(encodedInput);
+// console.log(encodedInput);
 var params = {
   text: encodedInput,
   tones: 'emotion'
 };
 
+var resultObj;
+
 tone_analyzer.tone(params, function(error, response) {
   if (error)
     console.log('error:', error);
   else
-    console.log(JSON.stringify(response, null, 2));
-    $('.responseText').val(response);
+    resultStr = JSON.stringify(response, null, 2);
+    // console.log(JSON.stringify(response, null, 2));
+    resultObj = response;
+    // console.log(response);
+    // console.log(resultObj.document_tone.tone_categories["0"].tones[1].score)
+    $('.responseText').append("<p class='text-center'>" + resultStr + "</p>");
   }
 );
 
--- bundle.js
+++ bundle.js
@@ -1,7 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 //created by Michael Harrington
 //encodeURI("string here"); will output string of type needed by watson
-
 var unique = require('uniq');
 
 var encodedInput = "";
@@ -10,13 +9,14 @@ $('#mainButton').click(function(){
   var input = $('#mainInput').val();
   encodedInput = encodeURI(input);
   encodedInput = '"' + encodedInput + '"'
-  console.log(encodedInput);
+  // console.log(encodedInput);
 
 // {
 //   "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
 //   "username": "aa44c815-f9e5-45ec-9862-86133587c17a",
 //   "password": "Zzgmqx7IQjf8"
 // }
+
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3.js');
 var tone_analyzer = new ToneAnalyzerV3({
   username: "d9eebd1b-b1a9-4cd9-94e3-714537281c54",
@@ -24,18 +24,24 @@ var tone_analyzer = new ToneAnalyzerV3({
   version_date: '2016-05-19'
 });
 
-console.log(encodedInput);
+// console.log(encodedInput);
 var params = {
   text: encodedInput,
   tones: 'emotion'
 };
 
+var resultObj;
+
 tone_analyzer.tone(params, function(error, response) {
   if (error)
     console.log('error:', error);
   else
-    console.log(JSON.stringify(response, null, 2));
-    $('.responseText').val(response);
+    resultStr = JSON.stringify(response, null, 2);
+    // console.log(JSON.stringify(response, null, 2));
+    resultObj = response;
+    // console.log(response);
+    // console.log(resultObj.document_tone.tone_categories["0"].tones[1].score)
+    $('.responseText').append("<p class='text-center'>" + resultStr + "</p>");
   }
 );
 
