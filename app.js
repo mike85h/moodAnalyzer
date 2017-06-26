c847e424bbb6a9a286c35c7b9f8095e9d1b44d3e
Michael Harrington <mike85h@gmail.com>
2017-06-25 18:05:31 -0600

    added h1, input, button, jqueryCDN, app.js. all working.

---
+++ app.js
@@ -0,0 +1,12 @@
+//created by Michael Harrington
+
+//encodeURI("string here"); will output string of type needed by watson
+
+
+
+
+// function getMood() {
+//   document.getElementById('main');
+// }
+
+console.log("Working MH")
