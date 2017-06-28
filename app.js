f962af0da0cd076c52502962231f5d75f3e111a7
Michael Harrington <mike85h@gmail.com>
2017-06-28 11:23:18 -0600

    3 Charts added. Chart customization in progress. All working.

--- app.js
+++ app.js
@@ -101,7 +101,124 @@ $("document").ready(function() {
               $('.resultBox').append("<div class='col-md-4 socResultColumn'></div>");
                 $('.socResultColumn').append("<h3>Social</h3>");
                 $('.socResultColumn').append("<p class='text-center' id='socResult'>" + socialResultStr + "</p>");
-      
+
+
+        //em chart
+        var ctx = document.getElementById("emChart").getContext('2d');
+        var emChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: ["Anger", "Disgust", "Fear", "Joy", "Sadness"],
+              datasets: [{
+                label: 'Emotional',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'Red',
+                  'Brown',
+                  'Yellow',
+                  'Green',
+                  'Purple',
+                ],
+                borderColor: [
+                  'Red',
+                  'Brown',
+                  'Yellow',
+                  'Green',
+                  'Purple',
+                ],
+                  borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true
+                      }
+                  }]
+              }
+          }
+      });
+
+      //lang chart
+      var ctx = document.getElementById("langChart").getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ["Analytical", "Confident", "Tentative"],
+            datasets: [{
+              label: 'Language',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+                borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    // soc chart
+    var ctx = document.getElementById("socChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Emotional Range"],
+          datasets: [{
+            label: 'Social',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+              borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+//end chart
+
     });
   });
 });
